5b494a45f7567da8abd96eeca234de6c8d56e3c2
dfhincapiem <dfhincapiem@unal.edu.co>
2017-04-16 19:34:44 -0500

    Se organiza el archivo app-routing de manera que sea mas legible

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,46 +3,27 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
-import { routing, appRoutingProviders } from './app.routing'
-
-import { AppComponent } from './app.component';
-import { LoginComponent } from './Components/home/home.component';
-import { AboutusComponent } from './Components/aboutus/aboutus.component';
-import { ResetComponent } from './Components/reset/reset.component';
-import { BarComponent } from './Components/bar/bar.component';
-import { EmployeesComponent } from './Components/employees/employees.component';
-import { NovetiesComponent } from './Components/noveties/noveties.component';
-import { LiquidationsComponent } from './Components/liquidations/liquidations.component';
-import { StatisticsComponent } from './Components/statistics/statistics.component';
-import { ParametersComponent } from './Components/parameters/parameters.component';
 import { EmployeesService } from './Services/employees/employees.service';
 import { LiquidationsService } from './Services/liquidations/liquidations.service';
 import { ParametersService } from './Services/parameters/parameters.service';
 import { NovetiesService } from  './Services/noveties/noveties.service';
 import { StatisticsService } from  './Services/statistics/statistics.service';
 
+import { routingComponent, AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent,
-    LoginComponent,
-    AboutusComponent,
-    ResetComponent,
-    BarComponent,
-    EmployeesComponent,
-    NovetiesComponent,
-    LiquidationsComponent,
-    StatisticsComponent,
-    ParametersComponent
+    AppComponent, routingComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule,
-    routing
+    AppRoutingModule
   ],
-  providers: [ appRoutingProviders,
+  providers: [ 
               EmployeesService,
               LiquidationsService,
               ParametersService,
