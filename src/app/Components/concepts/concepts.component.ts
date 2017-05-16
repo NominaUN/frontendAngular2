8d0afee23bc729ab8d8c3311312045e6133158c6
Andres Ceballos <afceballosr@unal.edu.co>
2017-05-16 15:55:34 -0500

    unificación de vistas

--- src/app/Components/concepts/concepts.component.ts
+++ src/app/Components/concepts/concepts.component.ts
@@ -1,44 +1,39 @@
 import { Component, OnInit } from '@angular/core';
 import { ConceptsService } from  '../../Services/concepts/concepts.service';
 import { Concept } from '../../Models/concept'
-
+import { Observable } from 'rxjs/Rx';
 
 @Component({
   selector: 'app-concepts',
   templateUrl: './concepts.component.html',
   styleUrls: ['./concepts.component.css']
 })
+
 export class ConceptsComponent implements OnInit {
 
-  concepts: Concept[];
-  concept = new Concept;
-
-  constructor(private conceptService: ConceptsService) {
-  }
+	concepts: Concept[];
+	concept = new Concept;
+	constructor(private conceptService: ConceptsService) {
+	}
  
-  loadConcepts(){
-	this.conceptService.getConcepts().subscribe(
-		(resConceptData => this.concepts = resConceptData)
-    ); 
-  }
+	loadConcepts(){
+		this.conceptService.getConcepts().subscribe(
+			(resConceptData => this.concepts = resConceptData)
+		); 
+	}
   
-  ngOnInit() {
-    this.loadConcepts();   
-  }
+	ngOnInit() {
+		this.loadConcepts();   
+	}
   
-  onSelect(concept:any):void {
-  	console.log(concept);
-  }
+	onSelect(concept:any):void {
+		console.log(concept);
+	}
 
-  createArea(concept : Concept){
-
+	createConcept(concept : Concept){
   		this.conceptService.setConcepts(concept).subscribe(
-            data => console.log('Success uploading the area', data),
+            data => console.log('Success uploading the concept', data),
             error => console.error(`Error: ${error}`), ()=>this.loadConcepts());
-
-
-
   	}
-  
 
 }
--- src/app/Services/concepts/concepts.service.ts
+++ src/app/Services/concepts/concepts.service.ts
@@ -6,22 +6,22 @@ import { Observable } from 'rxjs/Rx'
 @Injectable()
 export class ConceptsService {
 
-  private urlget : string = "http://localhost:3000/api/v1/concepts.json";
-  private urlpost : string = "http://localhost:3000/api/v1/concepts"
-  headers: Headers;
-  options: RequestOptions;
+	private urlget : string = "http://localhost:3000/api/v1/concepts.json";
+	private urlpost : string = "http://localhost:3000/api/v1/concepts"
+	headers: Headers;
+	options: RequestOptions;
 
-	
     constructor(private http: Http) { 
 		this.headers = new Headers({ 'Content-Type': 'application/json' });
         this.options = new RequestOptions({ headers: this.headers });
-
 	}
+	
 	getConcepts(): Observable<Concept[]> {
 	    return this.http.get(this.urlget).map((response:Response) => <Concept[]>response.json().data);
 	}
+	
 	setConcepts(concept : Concept){
         return this.http.post(this.urlpost, JSON.stringify(concept), this.options).map(response => response.json())
-
 	}
+	
 }
